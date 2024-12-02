6a173510ec5a457c0734f94683dfd9afcef3098c
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2024-12-02 16:29:11 -0300

    1.2
    Correção de alguns bugs

--- EX 065Gg.c
+++ EX 065Gg.c
@@ -2,30 +2,34 @@
 
 int main(void){
 
-    int num = 0, maior = 0, menor = 0, soma = 0, media = 0;
+    int num = 0, maior, menor, soma = 0, media = 0;
     int termos = 0;
  
     printf("Digite um numero:[999] para parar]\n");
     scanf("%d",&num);
 
-while(num != 999){
-    termos++;
     maior = num;
-    if (num > maior)
-        num = maior;
-    else
-        num = menor; 
+    menor = num;
 
+while(num != 999){
+    termos++; 
     soma += num;
     media = soma/termos;
+
+    if(num > maior){
+        maior = num;
+    }
+    if(num < menor)
+        menor = num;
+        
     
     printf("Digite um numero:[999] para parar]\n");
     scanf("%d",&num);}       
 
-    printf("FEEDBACK FINAL\n");
+    printf("\nFEEDBACK FINAL\n");
     printf("Soma dos numeros = %d\n",soma);
     printf("Media = %d\n",media);
-    printf("Maior e menor numeros = %d  / %d \n");
+    printf("Maior e menor numeros = %d  / %d \n",maior, menor);
 
     return 0;
 }
