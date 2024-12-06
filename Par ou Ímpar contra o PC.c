8dc6958520a709489be8cec305a7c3c469048646
dolbs <pedrorsouzaa032@gmail.com>
2024-12-06 12:56:17 -0300

    Update Par ou Ímpar contra o PC.c
    Mudança no sorteador de numeros e comentarios

--- Par ou Ímpar contra o PC.c
+++ Par ou Ímpar contra o PC.c
@@ -9,27 +9,31 @@ int main(){
     printf("JOGO DO PAR OU IMPAR\n");
 
     while(1){
-        printf("\nDigite um numero\n");
+        printf("\nDigite um numero entre 0 e 10\n");
         scanf("%d",&num);
 
         printf("Par ou Impar? [P/I]\n");
         scanf(" %c",&opcao);
 
+        //Sorteia um numero aleatório entre 0 e 10
         srand(time(NULL));
-        numPC = rand() % 10 + 1;
+        numPC = rand() % 11;
 
         soma = num + numPC;
 
         printf("O Computador escolheu %d\n", numPC);
         printf("Total %d\n", soma);
 
+        //Jogador Vence escolhendo Par
         if((opcao == 'p' || opcao == 'P') && soma % 2 == 0){
             printf("RESULTADO: PAR\nJogador Ganhou!\n");
             total++;}
+        //Jogador Vence escolhendo Impar    
         else 
             if((opcao == 'i' || opcao == 'I') && soma % 2 == 1){
                 printf("RESULTADO: IMPAR\nJogador Ganhou!\n");
                 total++;}
+        //Computador Vence e encerra o programa        
         else{
             printf("Computador Ganhou. Fim de Jogo\n");
             break;}
